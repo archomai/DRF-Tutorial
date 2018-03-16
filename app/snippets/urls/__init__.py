ac8748c513f7956eb65ec759cdda0038e603c192
Seungri Cho <pseudo.circuit@gmail.com>
2018-03-16 16:30:18 +0900

    view generic 구현

--- app/snippets/urls/__init__.py
+++ app/snippets/urls/__init__.py
@@ -5,6 +5,7 @@ app_name = 'snippets'
 urlpatterns = [
     path('api-view/', include('snippets.urls.api_view')),
     path('mixins/', include('snippets.urls.mixins')),
+    path('generic/', include('snippets.urls.generic')),
 
 ]
 
